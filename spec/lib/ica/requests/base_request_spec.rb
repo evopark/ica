a8b627734d7db928cc88eabf65365fcb623ed6bb
Marcus Ilgner <marcus.ilgner@evopark.de>
2017-09-05 17:59:15 +0200

    Explicitly set per-operation timeouts

---
+++ spec/lib/ica/requests/base_request_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'ica/requests/base_request'
+
+RSpec.describe ICA::BaseRequest do
+  let(:garage_system) { build(:garage_system) }
+  subject { described_class.new(garage_system) }
+
+  describe '#http' do
+    let(:http) { subject.send(:http) }
+
+    it 'sets per-operation timeouts' do
+      # unfortunately there's no public API to interrogate it about the timeouts
+      timeout_class, configuration = http.instance_eval do
+        [@default_options.timeout_class,
+         @default_options.timeout_options]
+      end
+      expect(timeout_class).to eq(HTTP::Timeout::PerOperation)
+      expect(configuration).to eq(read_timeout: 60,
+                                  write_timeout: 30,
+                                  connect_timeout: 5)
+    end
+  end
+end
